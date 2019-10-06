69b77a2a943bef926098e298ac3e61aa03d452c1
Erwin Sturluson <angelmercuries@gmail.com>
2019-10-07 01:47:17 +0300

    Added overload for GenerateSimpleUrl.

--- ErwMvcExtensions/Html/ErwUrlHelper.cs
+++ ErwMvcExtensions/Html/ErwUrlHelper.cs
@@ -4,6 +4,10 @@ namespace ErwMvcExtensions.Html
 {
     public static class ErwUrlHelper
     {
+        public static string GenerateSimpleUrl(object routeValues)
+        {
+            return GenerateSimpleUrl(new RouteValueDictionary(routeValues));
+        }
         public static string GenerateSimpleUrl(RouteValueDictionary routeValues)
         {
             string generatedUrl = "/" + (routeValues["area"] != null ?
